7bb63bb242aa5f09ae01c93c8d19b472cad082c0
tran.duc.minh <tran.duc.minh@moneyforward.co.jp>
2021-11-10 23:13:04 +0700

    init project

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resource :sessions, only: [:create, :destroy] 
+    end  
+  end
+end
