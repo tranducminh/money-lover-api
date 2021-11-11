05450e0f2aeff541388bf782ffb043f8c41e1115
tran.duc.minh <tran.duc.minh@moneyforward.co.jp>
2021-11-11 22:52:59 +0700

    feat: add validation to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true,
+    length: {
+      maximum: Settings.validations.user.name.max_length,
+      minimum: Settings.validations.user.name.min_length
+    }
+  validates :email, presence: true,
+    length: {maximum: Settings.validations.user.email.max_length},
+    format: {with: Settings.validations.user.email.regex},
+    uniqueness:  {case_sensitive: false}
+  validates :password, presence: true,
+    length: {minimum: Settings.validations.user.password.min_length}
 end
--- app/models/user_wallet.rb
+++ app/models/user_wallet.rb
@@ -1,4 +1,6 @@
 class UserWallet < ApplicationRecord
   belongs_to :user
   belongs_to :wallet
+
+  validates :user_role, inclusion: {presence: true, in: User::roles}
 end
